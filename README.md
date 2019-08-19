16c1e91498531eeb266eae60b28f7b201c9db4dd
brj1 <brj1@sfu.ca>
2019-08-19 11:49:43 -0700

    added README; changed license

---
+++ README.md
@@ -0,0 +1,31 @@
+#PaddedTipDates
+
+BEAST2 package that contains operators that having padding to avoid creating negative branches and can change ancetral node dates if need.
+
+##Installation
+
+1. your BEAST2 library folder given below based on your operating system.
+
+MACOS: ~/Library/Application Support/BEAST/2.5
+
+2. Unzip dist/PaddedTipDates.addon.zip into.
+
+3. Run BEAUTi2.
+
+4. Close BEAUTi2.
+
+##Usage
+
+To use the padded or recursive tip date operators in the this package first create a BEAST2 XML file with Tip Date sampling.
+
+Then edit the XML in a text editor:
+
+1. In the first line of the XML file replace either
+
+  * `required=""` with `required="TipDates v1.0.1"`
+  * `required="SomePackages"` with `required="TipDates v1.0.1:SomePackages"`
+   
+2. Replace each instance of `TipDatesRandomWalker` with either `TipDatesRandomWalkerPadded` or `TipDatesRandomWalkerRecursive`.
+  
+  * In the same line you can add `Padding="SomeNumber"` to change the padding to SomeNumber (default is 1e-4).
+  * For TipDatesRandomWalkerRecursive, you can also add `depthPenalty="SomeNumber"` to add set a penalty for increased depth (default is 0).
--- src/beast/evolution/operators/TipDatesRandomWalkerRecursive.java
+++ src/beast/evolution/operators/TipDatesRandomWalkerRecursive.java
@@ -8,19 +8,18 @@
 * information regarding copyright ownership and licensing.
 *
 * PaddedTipDates is free software; you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation; either version 2
+* it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 3
 * of the License, or (at your option) any later version.
 *
 *  PaddedTipDates is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU Lesser General Public License for more details.
+*  GNU General Public License for more details.
 *
-* You should have received a copy of the GNU Lesser General Public
-* License along with PaddedTipDates; if not, write to the
-* Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-* Boston, MA  02110-1301  USA
+* You should have received a copy of the GNU General Public
+* License along with PaddedTipDates; if not it can be found at
+* https://www.gnu.org/licenses/gpl-3.0.html.
 */
 package beast.evolution.operators;
 
@@ -32,11 +31,9 @@ import beast.util.Randomizer;
  * pads TipDatesRandomWalker so that edges will not haver negative branches
  * @author Bradley R. Jones
  */
-public class TipDatesRandomWalkerRecursive extends TipDatesRandomWalker {
+public class TipDatesRandomWalkerRecursive extends TipDatesRandomWalkerPadded {
     final public Input<Double> depthPenaltyInput = new Input<>("depthPenalty", "penalty for shifting parent nodes");
-    final public Input<Double> paddingInput = new Input<>("padding", "amount of padding to ensure that edges have nonnegative length");
-            
-    double padding;            
+
     double depthPenalty;
     
     @Override
@@ -49,12 +46,6 @@ public class TipDatesRandomWalkerRecursive extends TipDatesRandomWalker {
             depthPenalty = depthPenaltyInput.get();
         } else {
             depthPenalty = 0;
-        }
-        
-        if (paddingInput.get() != null) {
-            padding = paddingInput.get();
-        } else {
-            padding = 1E-4;
         }
     }
     
